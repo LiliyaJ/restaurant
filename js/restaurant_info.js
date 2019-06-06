f2214da3684422f31dd51e054dd5a5643f91de26
Liliya Jeromin <zl108108@gmail.com>
2019-06-06 22:59:50 +0200

    API

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<pk.eyJ1IjoibGlsaXlhaiIsImEiOiJjandqbnl4bTkwbHNkNDBuNnQ5aW5lc2J0In0.2x5Bk5jGDdCdz4rs6kaYVw>',
+        mapboxToken: '<AIzaSyDpjMOWpt-hYSV5l1Hj9EKgNJhShAASQIM>',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
